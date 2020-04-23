ce8bad0474790e1fdb82012ec436b0d817069a28
marcelomnc <marcelo@pig.gi>
2020-04-23 13:06:50 -0500

    Routes and components addition. :bowtie:

--- src/App.js
+++ src/App.js
@@ -2,44 +2,99 @@ import React from "react";
 import "./assets/styles/App.css";
 import "./assets/styles/LoadingSpinner.css";
 import { Route, Switch, Redirect, withRouter } from "react-router-dom";
-import GlobalComparison from "./components/GlobalComparison/GlobalComparison";
-import CountryComparison from "./components/CountryComparison/CountryComparison";
-import GvCComparisonCummulative from "./components/GvCComparison/Cummulative/Cummulative";
-import GvCComparisonToday from "./components/GvCComparison/Today/Today";
-import Last10DaysMyCountry from "./components/Last10Days/MyCountry/MyCountry"
+import ABComparison from "./components/ABComparison/ABComparison";
+import * as ABComparisonTypes from "./components/ABComparison/Types";
+import Last10Days from "./components/Last10Days/Last10Days";
 import Layout from "./hoc/Layout/Layout";
 import Modal from "./components/ui/Modal/Modal";
-import LoadingSpinner from './components/ui/LoadingSpinner/LoadingSpinner'
+import LoadingSpinner from "./components/ui/LoadingSpinner/LoadingSpinner";
 
 function App() {
 	let routes = (
 		<Switch>
-			<Route path="/" exact component={GlobalComparison} />
-			<Route path="/summary/global" exact component={GlobalComparison} />
-			<Route path="/summary/country" exact component={CountryComparison} />
+			<Route
+				path="/"
+				exact
+				render={() => {
+					return (
+						<ABComparison
+							aBComparisonType={
+								ABComparisonTypes.GLOBAL_CUMMULATIVE_VS_TODAY_COMPARISON
+							}
+						/>
+					);
+				}}
+			/>
+			<Route
+				path="/summary/global"
+				exact
+				render={() => {
+					return (
+						<ABComparison
+							aBComparisonType={
+								ABComparisonTypes.GLOBAL_CUMMULATIVE_VS_TODAY_COMPARISON
+							}
+						/>
+					);
+				}}
+			/>
+			<Route
+				path="/summary/country"
+				exact
+				render={() => {
+					return (
+						<ABComparison
+							aBComparisonType={
+								ABComparisonTypes.COUNTRY_CUMMULATIVE_VS_TODAY_COMPARISON
+							}
+						/>
+					);
+				}}
+			/>
 			<Route
 				path="/summary/gvc/cummulative"
 				exact
-				component={GvCComparisonCummulative}
+				render={() => {
+					return (
+						<ABComparison
+							aBComparisonType={
+								ABComparisonTypes.GLOBAL_VS_COUNTRY_CUMMULATIVE_COMPARISON
+							}
+						/>
+					);
+				}}
 			/>
-			<Route path="/summary/gvc/today" exact component={GvCComparisonToday} />
-			<Route path="/lastDays/myCountry" exact component={Last10DaysMyCountry} />
+			<Route
+				path="/summary/gvc/today"
+				render={() => {
+					return (
+						<ABComparison
+							aBComparisonType={
+								ABComparisonTypes.GLOBAL_VS_COUNTRY_TODAY_COMPARISON
+							}
+						/>
+					);
+				}}
+			/>
+			<Route
+				path="/lastDays/myCountry"
+				exact
+				render={() => {
+					return <Last10Days />;
+				}}
+			/>
 			<Route
 				path="/test"
 				exact
 				render={() => {
 					return (
-						<Modal show={true} onClickHandler={() => alert('OELO')}>
+						<Modal show={true} onClickHandler={() => alert("OELO")}>
 							<h1>Esto es un titulo en el modal</h1>
 						</Modal>
 					);
 				}}
 			/>
-			<Route
-				path="/testSpinner"
-				exact
-				component={LoadingSpinner}
-			/>
+			<Route path="/testSpinner" exact component={LoadingSpinner} />
 			<Redirect to="/" />
 		</Switch>
 	);
