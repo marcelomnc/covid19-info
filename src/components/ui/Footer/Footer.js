8304a1d631babbe5bf0480ae3c8e64c50497b8d5
marcelomnc <marcelo@pig.gi>
2020-05-16 12:51:26 -0500

    Footer legend change. :bowtie:

--- src/components/ui/Footer/Footer.js
+++ src/components/ui/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = (props) => {
 	return (
 		<footer className="Footer">
-			<h5>Covid19-Stats App &copy; Wounded Dragons All rights reserved</h5>
+			<h5>Developed by &copy;Wounded Dragons All rights reserved</h5>
 		</footer>
 	);
 };
