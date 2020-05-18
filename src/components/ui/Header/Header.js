1437b252071f9cf76e49e38bcb5bd85a6ac719db
marcelomnc <marcelo@pig.gi>
2020-05-18 11:41:35 -0500

    TitleBar unused component removal. :bowtie:

--- src/components/ui/Header/Header.js
+++ src/components/ui/Header/Header.js
@@ -4,7 +4,6 @@ import NavigationBar from "./NavigationBar/NavigationBar";
 const Header = (props) => {
 	return (
 		<header className="Header">
-			<TitleBar />
 			<NavigationBar />
 		</header>
 	);
