a06256b04a390af502c14b1172b79b585943e214
marcelomnc <marcelo@pig.gi>
2020-05-19 12:28:24 -0500

    Footer title change. :bowtie:

--- src/components/ui/Footer/Footer.js
+++ src/components/ui/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = (props) => {
 	return (
 		<footer className="Footer">
-			<h5>Desarrollado por &copy;Wounded Dragons. Todos los derechos reservados</h5>
+			<h5>Desarrollado por &copy;Wounded Dragons&reg;</h5>
 		</footer>
 	);
 };
