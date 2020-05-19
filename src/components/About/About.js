3288b6673764ff2afacbb0aa3ffcd903fdd03587
marcelomnc <marcelo@pig.gi>
2020-05-19 12:21:15 -0500

    About page skeleton addition. :bowtie:

---
+++ src/components/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = props => {
+    return (
+        <div className="About">
+            <p>Esta será la página de acerca de nostros !</p>
+        </div>
+    )
+}
+
+export default About
