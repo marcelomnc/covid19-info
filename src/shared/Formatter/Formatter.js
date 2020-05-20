6a1206579ca6a2d5803ed7286b9e579000144e20
marcelomnc <marcelo@pig.gi>
2020-05-20 15:48:51 -0500

    Logic addition to format dates. :bowtie:

--- src/shared/Formatter/Formatter.js
+++ src/shared/Formatter/Formatter.js
@@ -1,1 +1,6 @@
 export const formatNumber = (number) => number.toLocaleString();
+
+export const formatDate = (strDate) => {
+	const date = new Date(strDate);
+	return date.toISOString().slice(0, 10);
+};
