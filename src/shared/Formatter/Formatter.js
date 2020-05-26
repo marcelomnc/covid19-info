b0e96de6f3aab677653571912dc03ecabd2406ec
marcelomnc <marcelo@pig.gi>
2020-05-26 16:30:46 -0500

    Logic addition to format percentage values. :bowtie:

--- src/shared/Formatter/Formatter.js
+++ src/shared/Formatter/Formatter.js
@@ -4,3 +4,8 @@ export const formatDate = (strDate) => {
 	const date = new Date(strDate);
 	return date.toISOString().slice(0, 10);
 };
+
+export const formatPercentage = (number) => {
+	const formattedNumber = formatNumber(number.toFixed(2));
+	return `${formattedNumber}%`;
+};
