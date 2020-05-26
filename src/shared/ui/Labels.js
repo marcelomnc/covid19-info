edc72884fc8ee19b20f4d6dced6dce4825f7f397
marcelomnc <marcelo@pig.gi>
2020-05-26 16:20:38 -0500

    Component creation to extract the app labels. :bowtie:

---
+++ src/shared/ui/Labels.js
@@ -0,0 +1,28 @@
+export const appLabel = "Covid19-Stats";
+export const summaryLabel = "Resumen";
+export const globalLabel = "Global";
+export const colombiaLabel = "Colombia";
+export const globalVsColombiaLabel = globalLabel + " vs " + colombiaLabel;
+export const lastDaysColombiaLabel = "Últimos días " + colombiaLabel;
+export const aboutLabel = "Acerca de";
+export const dateLabel = "Fecha";
+export const activeLabel = "Activos";
+export const recoveredLabel = "Recuperados";
+export const deathsLabel = "Muertos";
+export const confirmedLabel = "Confirmados";
+export const cumulativeLabel = "Acumulado";
+export const newCasesLabel = "Nuevos Casos";
+export const globalCumulativeVsTodayEntityATitle =
+	cumulativeLabel + " - Global";
+export const globalCumulativeVsTodayEntityBTitle = newCasesLabel + " - Global";
+export const countryCumulativeVsTodayEntityATitle =
+	cumulativeLabel + " - Colombia";
+export const countryCumulativeVsTodayEntityBTitle =
+	newCasesLabel + " - Colombia";
+export const globalVsCountryCumulativeEntityATitle =
+	cumulativeLabel + " - Global";
+export const globalVsCountryCumulativeEntityBTitle =
+	cumulativeLabel + " - Colombia";
+export const globalVsCountryNewCasesEntityATitle = newCasesLabel + " - Global";
+export const globalVsCountryNewCasesEntityBTitle =
+	newCasesLabel + " - Colombia";
