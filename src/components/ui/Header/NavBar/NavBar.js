3b9b9f4fa9456788840aed0418ea23c934b890f3
marcelomnc <marcelo@pig.gi>
2020-05-26 16:20:57 -0500

    Use of extracted labels. :bowtie:

--- src/components/ui/Header/NavBar/NavBar.js
+++ src/components/ui/Header/NavBar/NavBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 import logo from "../../../../assets/images/AppLogo.svg";
+import * as Labels from "../../../../shared/ui/Labels";
 
 const NavBar = (props) => {
 	return (
@@ -12,38 +13,38 @@ const NavBar = (props) => {
 					width="30"
 					height="30"
 					className="NavBar__brand__logo d-inline-block align-top"
-				/>{" "}
-				Covid19-Stats
+				/>
+				{" " + Labels.appLabel}
 			</Navbar.Brand>
 			<Navbar.Toggle aria-controls="basic-navbar-nav" />
 			<Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
 				<Nav className="Nav">
-					<NavDropdown title="Resumen">
+					<NavDropdown title={Labels.summaryLabel}>
 						<NavDropdown.Item eventKey="1.1" href="/summary/global">
-							Global
+							{Labels.globalLabel}
 						</NavDropdown.Item>
 						<NavDropdown.Item eventKey="1.2" href="/summary/country">
-							Colombia
+							{Labels.colombiaLabel}
 						</NavDropdown.Item>
 					</NavDropdown>
-					<NavDropdown title="Global vs Colombia">
-						<NavDropdown.Item eventKey="2.1" href="/gvc/cummulative">
-							Acumulado
+					<NavDropdown title={Labels.globalVsColombiaLabel}>
+						<NavDropdown.Item eventKey="2.1" href="/gvc/cumulative">
+							{Labels.cumulativeLabel}
 						</NavDropdown.Item>
 						<NavDropdown.Item eventKey="2.2" href="/gvc/newCases">
-							Nuevos Casos
+							{Labels.newCasesLabel}
 						</NavDropdown.Item>
 					</NavDropdown>
-					<NavDropdown title="Últimos días Colombia">
-						<NavDropdown.Item eventKey="3.1" href="/lastDays/cummulative">
-							Acumulado
+					<NavDropdown title={Labels.lastDaysColombiaLabel}>
+						<NavDropdown.Item eventKey="3.1" href="/lastDays/cumulative">
+							{Labels.cumulativeLabel}
 						</NavDropdown.Item>
 						<NavDropdown.Item eventKey="3.2" href="/lastDays/newCases">
-							Nuevos Casos
+							{Labels.newCasesLabel}
 						</NavDropdown.Item>
 					</NavDropdown>
 					<Nav.Link eventKey="4" href="/about">
-						Acerca de
+						{Labels.aboutLabel}
 					</Nav.Link>
 				</Nav>
 			</Navbar.Collapse>
