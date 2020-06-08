331c792f4a2d9a927afd0f6b8537a4e4d5e41361
marcelomnc <marcelo@pig.gi>
2020-06-08 10:02:03 -0500

    Footer unnecessary tag removal. :bowtie:

--- src/components/ui/Footer/Footer.js
+++ src/components/ui/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = (props) => {
 	return (
 		<footer className="Footer">
-			<h5>Desarrollado por &copy;Wounded Dragons&reg;</h5>
+			Desarrollado por &copy;Wounded Dragons&reg;
 		</footer>
 	);
 };
